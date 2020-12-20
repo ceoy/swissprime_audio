07cbdef02c8f36cf2750cf8a6c5507f26f2642da
Tim Jahn <ceoymw@gmail.com>
2020-12-20 19:41:32 +0100

    adds initial version

---
+++ SwissPrime_Audio.lua
@@ -0,0 +1,4 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+LSM:Register("sound", "Thomas Dodge", [[Interface\AddOns\SwissPrime_Audio\Media\Audio\Dodge.mp3]])
+LSM:Register("sound", "Thomas Achtung", [[Interface\AddOns\SwissPrime_Audio\Media\Audio\Care.mp3]])
