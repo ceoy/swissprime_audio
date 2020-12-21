5d500c09687400b2c10b1cc80d3392ddbd2ec19c
Tim Jahn <ceoymw@gmail.com>
2020-12-21 12:56:06 +0100

    adds updated sounds

--- SwissPrime_Audio.lua
+++ SwissPrime_Audio.lua
@@ -1,4 +1,9 @@
 local LSM = LibStub("LibSharedMedia-3.0") 
 
-LSM:Register("sound", "Thomas Dodge", [[Interface\AddOns\SwissPrime_Audio\Media\Audio\Dodge.mp3]])
-LSM:Register("sound", "Thomas Achtung", [[Interface\AddOns\SwissPrime_Audio\Media\Audio\Care.mp3]])
+LSM:Register("sound", "Swiss Achtung", [[Interface\AddOns\SwissPrime_Audio\Media\Audio\Achtung.mp3]])
+LSM:Register("sound", "Swiss Dodge", [[Interface\AddOns\SwissPrime_Audio\Media\Audio\Dodge.mp3]])
+LSM:Register("sound", "Swiss Interrupt", [[Interface\AddOns\SwissPrime_Audio\Media\Audio\Interrupt.mp3]])
+LSM:Register("sound", "Swiss Out", [[Interface\AddOns\SwissPrime_Audio\Media\Audio\Out.mp3]])
+LSM:Register("sound", "Swiss Ready", [[Interface\AddOns\SwissPrime_Audio\Media\Audio\Ready.mp3]])
+LSM:Register("sound", "Swiss Soak", [[Interface\AddOns\SwissPrime_Audio\Media\Audio\Soak.mp3]])
+LSM:Register("sound", "Swiss Soak All", [[Interface\AddOns\SwissPrime_Audio\Media\Audio\SoakAll.mp3]])
